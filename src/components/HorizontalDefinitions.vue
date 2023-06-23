9237f56e4c049f2ea199a70171a6f6941f02ca96
misu <szijjartonagy.misu@gmail.com>
2023-06-23 23:11:14 +0200

    use computed for definitions

--- src/components/HorizontalDefinitions.vue
+++ src/components/HorizontalDefinitions.vue
@@ -1,10 +1,14 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
     table: {
         type: Array,
         required: true,
     },
 });
+
+const definitions = computed(() => props.table.map(numsForRow));
 
 const numsForRow = (row) => {
     const nums = [];
@@ -28,8 +32,8 @@ const numsForRow = (row) => {
 
 <template>
     <section class="definitions horizontal">
-        <section class="row" v-for="row in table">
-            <article v-for="num in numsForRow(row)">{{ num }}</article>
+        <section class="row" v-for="definition in definitions">
+            <article v-for="num in definition">{{ num }}</article>
         </section>
     </section>
 </template>
