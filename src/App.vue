7bb3d72f0db76bf099d420a7c50d29a99e04b58f
misu <szijjartonagy.misu@gmail.com>
2023-06-23 22:40:47 +0200

    set size

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed, ref } from 'vue';
 
+import SetSize from './components/SetSize.vue';
 import NonogramTable from './components/NonogramTable.vue';
 
 
@@ -17,9 +18,28 @@ const changeColor = (rowIdx, colIdx) => {
     const color = table.value[rowIdx][colIdx];
     table.value[rowIdx][colIdx] = color === null ? "black" : null;
 };
+
+const getCellValue = (rowIdx, colIdx) => table.value[rowIdx] && table.value[rowIdx][colIdx];
+
+const setTable = (newRowCount, newColCount) => {
+    const newTable = [];
+    for (let rowIdx = 0; rowIdx < newRowCount; rowIdx++) {
+        newTable[rowIdx] = [];
+        for (let colIdx = 0; colIdx < newColCount; colIdx++) {
+            newTable[rowIdx][colIdx] = getCellValue(rowIdx, colIdx) || null;
+        }
+    }
+    table.value = newTable;
+};
 </script>
 
 <template>
+    <SetSize
+        :rowCount="rowCount"
+        :colCount="colCount"
+        @setRowCount="newRowCount => setTable(newRowCount, colCount)"
+        @setColCount="newColCount => setTable(rowCount, newColCount)"
+    />
     <NonogramTable
         :table="table"
         @cellClick="changeColor"
---
+++ src/components/SetSize.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+    colCount: {
+        type: Number,
+        required: true,
+    },
+    rowCount: {
+        type: Number,
+        required: true,
+    },
+});
+
+const emit = defineEmits(["setRowCount", "setColCount"]);
+
+const setRowCount = ($event) => emit("setRowCount", $event.target.value);
+const setColCount = ($event) => emit("setColCount", $event.target.value);
+</script>
+
+<template>
+    <fieldset>
+        Size:
+        <input type="number" :value="rowCount" @input="setRowCount" />
+        ×
+        <input type="number" :value="colCount" @input="setColCount" :disabled="rowCount === 0" />
+    </fieldset>
+</template>
+
+<style scoped>
+fieldset {
+    padding-inline-start: 0;
+    border: none;
+}
+
+input[type=number] {
+    width: 50px;
+}
+</style>
