561437f54c4380a52bd0a69ff81930ccca7ebc35
misu <szijjartonagy.misu@gmail.com>
2023-06-24 10:41:29 +0200

    adding welcome page

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import Welcome from './views/Welcome.vue';
 import Create from './views/Create.vue';
 
 const routes = [
     {
         name: "home",
         path: "/",
-        component: { template: "<div>Hello</div>" }, // TODO
+        component: Welcome,
     },
     {
         name: "create",
         path: "/create",
         component: Create,
+    },
+    {
+        name: "solve",
+        path: "/solve",
+        component: { template: "TODO" },
     },
 ];
 
---
+++ src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Nonogram</h1>
+    <p>
+        Welcome to my nonogram application where you can
+        <RouterLink :to="{ name: 'create' }">create</RouterLink>
+        or
+        <RouterLink :to="{ name: 'solve' }">solve</RouterLink>
+        nonogram puzzles.
+    </p>
+    <p>Have fun!</p>
+</template>
+
+<style scoped>
+</style>
